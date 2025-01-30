29a2c428329cc20d363351cc5ea98f4218d4f63c
Chris Rowe <krisroe@yahoo.com>
2025-01-30 17:31:42 -0600

    add base concepts for existence, add start song list

---
+++ Library.cs
@@ -0,0 +1,24 @@
+﻿namespace Existence
+{
+    internal static class Existence
+    {
+        public static class Library
+        {
+            public static class Entity { }
+            public static class Logic { }
+            public static class Intuition { }
+        }
+
+        public class Person { }
+        public class Human { }
+        public class Simulation { }
+        public class Universe { }
+        public class Galaxy { }
+        public class SolarSystem { }
+
+        public class Sun { }
+        public class World { }
+        public class Emotion { }
+        public class Ethics { }
+    }
+}
