1df3a309ddc0edc4b04c7ce901004e706e13d0d0
Chris Rowe <krisroe@yahoo.com>
2025-01-31 17:14:27 -0600

    add famous musicians and physicists

---
+++ Earth/People/Famous/Music/Examples.cs
@@ -0,0 +1,11 @@
+﻿
+namespace Existence.Earth.People.Famous.Music
+{
+    public static class Examples
+    {
+        private static class BillyJoel { }
+        private static class TaylorSwift { }
+        private static class EltonJohn { }
+        private static class JohnnyCash { }
+    }
+}
---
+++ Earth/People/Famous/Physics/Examples.cs
@@ -0,0 +1,34 @@
+﻿
+namespace Existence.Earth.People.Famous.Physics
+{
+    public static class Physics
+    {
+        private static class IsaacNewton 
+        {
+            private static class LawsOfMotion { }
+            private static class UniversalGravitation { }
+            private static class Calculus { }
+        }
+
+        private static class GalileoGalilei { }
+        private static class JamesClerkMaxwell { }
+        private static class MichaelFaraday { }
+        private static class JohannesKepler { }
+        private static class LeonhardEuler {}
+        private static class AlbertEinstein { }
+        private static class NielsBohr { }
+        private static class WernerHeisenberg { }
+        private static class ErwinSchrodinger { }
+        private static class RichardFeynman { }
+        private static class PaulDirac { }
+        private static class EnricoFermi { }
+        private static class MurrayGellMann { }
+        private static class MaxPlanck { }
+        private static class MarieCurie { }
+        private static class JRobertOppenheimer { }
+        private static class StephenHawking { }
+        private static class RogerPenrose { }
+        private static class PeterHiggs { }
+    }
+
+}
