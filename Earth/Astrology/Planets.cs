bc5dde3c045fecfaba07c33d191d80513e469a4e
Chris Rowe <krisroe@yahoo.com>
2025-01-31 07:26:54 -0600

    add list of countries, planets, fix whitespace issue with existence csproj (probably server squirrels)

---
+++ Earth/Astrology/Planets.cs
@@ -0,0 +1,32 @@
+﻿using System.Diagnostics;
+
+namespace Existence.Earth.Astrology
+{
+    public static class Planets
+    {
+        public static class Mercury { }
+        public static class Venus { }
+        public static class Earth { }
+        public static class Mars { }
+        public static class Jupiter { }
+        public static class Saturn { }
+        public static class Uranus
+        {
+            public static class Funny { }
+            public static class Sounding { }
+            public static class Name { }
+            public static class In { }
+            public static class English { }
+            public static class  Meaning
+            {
+                public static class Your { }
+                public static class Anus { }
+                public static class Or { }
+                public static class Urine { }
+                public static class Us { }
+            }
+        }
+        public static class Neptune { }
+        public static class Pluto { }
+    }
+}
---
+++ Earth/Countries.cs
@@ -0,0 +1,40 @@
+﻿namespace Existence.Earth
+{
+    public static class Countries
+    {
+        public static class UnitedStates { }
+        public static class Canada { }
+        public static class Mexico { }
+        public static class Russia { }
+        public static class China { }
+        public static class UnitedKingdom { }
+        public static class France { }
+        public static class Spain { }
+        public static class Argentina { }
+        public static class Brazil { }
+        public static class Japan { }
+        public static class Chile { }
+        public static class Mongolia { }
+        public static class Egypt { }
+        public static class Iceland { }
+        public static class Madagascar { }
+        public static class Denmark { }
+        public static class Norway { }
+        public static class Sweden { }
+        public static class Finland { }
+        public static class Germany { }
+        public static class India { }
+        public static class Syria { }
+        public static class Iraq { }
+        public static class Iran { }
+        public static class Panama { }
+        public static class Cuba { }
+        public static class Austria { }
+        public static class Belgium { }
+        public static class Bahamas { }
+        public static class Andorra { }
+        public static class Luxembourg { }
+        public static class Italy { }
+        public static class Ireland { }
+    }
+}
