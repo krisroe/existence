13502ee8ae56043dadb28f49a2ae5f2bf3bf4281
Chris Rowe <krisroe@yahoo.com>
2025-01-30 20:26:15 -0600

    add relatively ambiguous personal information

---
+++ Personal/Self.cs
@@ -0,0 +1,40 @@
+﻿namespace Existence.Personal
+{
+    internal static class Self
+    {
+        public static class Name
+        {
+            public static class Personal { }
+        }
+
+        public static class Man { }
+        public static class Male { }
+        public static class Spouse { }
+        public static class Husband { }
+
+        public static class Married
+        {
+            public static class Spouse { }
+            public static class Wife { }
+            public static class Woman { }
+            public static class Female { }
+        }
+
+        public static class Birth
+        {
+            public static class Year
+            {
+                public static class OneThousandNineHundredEighty { }
+            }
+            public static class Month
+            {
+                public static class December { }
+            }
+            public static class Day
+            {
+                public static class Six { }
+            }
+        }
+        public static class Sagittarius { }
+    }
+}
