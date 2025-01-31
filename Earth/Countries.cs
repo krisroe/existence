d9981e30017097de485ad3e2d61019fb4c918df1
Chris Rowe <krisroe@yahoo.com>
2025-01-31 07:55:25 -0600

    add country information

--- Earth/Countries.cs
+++ Earth/Countries.cs
@@ -1,40 +1,158 @@
 ﻿namespace Existence.Earth
 {
-    public static class Countries
+    public static class CountriesAlphabetical
     {
-        public static class UnitedStates { }
-        public static class Canada { }
-        public static class Mexico { }
-        public static class Russia { }
-        public static class China { }
-        public static class UnitedKingdom { }
-        public static class France { }
-        public static class Spain { }
-        public static class Argentina { }
-        public static class Brazil { }
-        public static class Japan { }
-        public static class Chile { }
-        public static class Mongolia { }
-        public static class Egypt { }
-        public static class Iceland { }
-        public static class Madagascar { }
-        public static class Denmark { }
-        public static class Norway { }
-        public static class Sweden { }
-        public static class Finland { }
-        public static class Germany { }
-        public static class India { }
-        public static class Syria { }
-        public static class Iraq { }
-        public static class Iran { }
-        public static class Panama { }
-        public static class Cuba { }
-        public static class Austria { }
-        public static class Belgium { }
-        public static class Bahamas { }
         public static class Andorra { }
-        public static class Luxembourg { }
-        public static class Italy { }
+        public static class Argentina { }
+        public static class Austria { }
+        public static class Bahamas { }
+        public static class Belize { }
+        public static class Belgium { }
+        public static class Bolivia { }
+        public static class BosniaAndHerzigovina { }
+        public static class Brazil { }
+        public static class Canada { }
+        public static class Chile { }
+        public static class China { }
+        public static class Croatia { }
+        public static class Cuba { }
+        public static class Denmark { }
+        public static class Egypt { }
+        public static class Finland { }
+        public static class France { }
+        public static class Germany { }
+        public static class Greece { }
+        public static class Hungary { }
+        public static class Iceland { }
+        public static class India { }
+        public static class Iran { }
+        public static class Iraq { }
         public static class Ireland { }
+        public static class Israel { }
+        public static class Italy { }
+        public static class Japan { }
+        public static class Kenya { }
+        public static class Laos { }
+        public static class Luxembourg { }
+        public static class Madagascar { }
+        public static class Mexico { }
+        public static class Mongolia { }
+        public static class Montenegro { }
+        public static class Morocco { }
+        public static class NorthMacedonia { }
+        public static class Norway { }
+        public static class Panama { }
+        public static class Paraguay { }
+        public static class Poland { }
+        public static class Russia { }
+        public static class Serbia { }
+        public static class Slovakia { }
+        public static class Slovenia { }
+        public static class SouthAfrica { }
+        public static class Spain { }
+        public static class Sweden { }
+        public static class Switzerland { }
+        public static class Syria { }
+        public static class UnitedKingdom { }
+
+
+        public static class UnitedStates
+        {
+            public static class PresidentListChronological
+            {
+                public static class GeorgeWashington { }
+                public static class JohnAdams { }
+                public static class ThomasJefferson { }
+                public static class JamesMadison { }
+                public static class JamesMonroe { }
+                public static class JohnQuincyAdams { }
+                public static class AndrewJackson { }
+                public static class MartinVanBuren { }
+                public static class WilliamHenryHarrison
+                {
+                    public static class DiedInOffice { }
+                }
+                public static class JohnTyler { }
+                public static class JamesKPolk { }
+                public static class ZacharyTaylor
+                {
+                    public static class DiedInOffice { }
+                }
+                public static class MillardFilmore { }
+                public static class FranklinPierce { }
+                public static class JamesBuchanan { }
+                public static class AbrahamLincoln
+                {
+                    public static class Assassinated { }
+                }
+                public static class AndrewJohnson { }
+                public static class UlyssesSGrant { }
+                public static class RutherfordBHayes { }
+                public static class JamesAGarfield
+                {
+                    public static class Assassinated { }
+                }
+                public static class ChesterAArthur { }
+                public static class GroverClevelandFirstTerm { }
+                public static class BenjaminHarrison { }
+                public static class GroverClevelandSecondTerm { }
+                public static class WilliamMcKinley
+                {
+                    public static class Assassinated { }
+                }
+                public static class TheodoreRoosevelt { }
+                public static class WilliamHowardTaft { }
+                public static class WoodrowWilson { }
+                public static class WarrenGHarding
+                {
+                    public static class DiedInOffice { }
+                }
+                public static class CalvinCoolidge { }
+                public static class HerbertHoover { }
+                public static class FranklinDRooseveltThreeConsecutiveTerms
+                {
+                    public static class DiedInOffice { }
+                }
+                public static class HarrySTruman { }
+                public static class DwightDEisenhowever { }
+                public static class JohnFKennedy
+                {
+                    public static class Assassinated { }
+                }
+                public static class LyndonBJohnson { }
+                public static class RichardNixon { }
+                public static class GeraldFord { }
+                public static class JimmyCarter { }
+                public static class RonaldReagan { }
+                public static class GeorgeHWBush { }
+                public static class BillClinton { }
+                public static class GeorgeWBush { }
+                public static class BarackObama { }
+                public static class DonaldTrumpFirstTerm { }
+                public static class JoeBiden { }
+                public static class DonaldTrumpSecondTerm { }
+            }
+        }
+
+        public static class Uruguay { }
+        public static class Venezuela { }
+        public static class Yemen { }
+        public static class Zimbabwe { }
+    }
+
+    public static class CountriesThatCouldSeparate
+    {
+        public class Example1
+        {
+            public static class China { }
+            public static class Taiwan { }
+        }
+    }
+
+    public static class NotYetCountries
+    {
+        public static class Kosovo { }
+        public static class Palestine { }
+        public static class Taiwan { }
     }
 }
