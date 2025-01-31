0db1bbf29c043917b486deec820a0d03ea9585a9
Chris Rowe <krisroe@yahoo.com>
2025-01-31 15:46:00 -0600

    add note about youtube channel, current and former employers, connection between pope and latin

--- Personal/Self.cs
+++ Personal/Self.cs
@@ -7,6 +7,15 @@
             public static class Personal { }
         }
 
+        public static class YouTubeChannel
+        {
+            public static class Destroy { }
+            public static class All { }
+            public static class Pugs { }
+            public static class Abuse { }
+            public static class RemindMeNotTo { }
+        }
+
         public static class Sagittarius { }
         public static class Man { }
         public static class Male { }
@@ -23,6 +32,41 @@
             public static class Female { }
             public static class Cancer { }
             public static class Alive { }
+        }
+
+        public static class Employers
+        {
+            public static class Current
+            {
+                public static class AbsoluteSolutionsInc { }
+                public static class ASI { }
+                public static class WTS { }
+                public static class Paradigm { }
+                public static class Automated { }
+                public static class AutomatedBuild { }
+                public static class Build { }
+                public static class BFS { }
+                public static class Builders { }
+                public static class First { }
+                public static class Source { }
+            }
+            public static class Past
+            {
+                public static class Epic { }
+                public static class Systems { }
+                public static class Corporation { }
+                public static class Health { }
+                public static class Care { }
+                public static class Failed
+                {
+                    public static class Software { }
+                    public static class Test { }
+                    public static class Engineer { }
+                }
+                public static class Inpatient { }
+                public static class HOV { }
+                public static class Ambulatory { }
+            }
         }
 
         public static class Birth
@@ -51,6 +95,12 @@
         public static class Death
         {
             public static class Latin { }
+            public static class Dead { }
+            public static class Language { }
+            public static class But { }
+            public static class Pope { }
+            public static class Knows { }
+            public static class Something { }
         }
     }
 }
