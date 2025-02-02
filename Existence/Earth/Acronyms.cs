012a71e8a3578c9e47958d09c501d294efb71653
Chris Rowe <destroyallpugs@gmail,com>
2025-02-02 08:00:38 -0600

    add UPS and IPT acronyms

---
+++ Existence/Earth/Acronyms.cs
@@ -0,0 +1,9 @@
+﻿
+namespace Existence.Earth
+{
+    public static class UPS
+    {
+        public static class UnitedParcelService { }
+        public static class PackageDelivery { }
+    }
+}
---
+++ Existence/Time/Continuum.cs
@@ -0,0 +1,38 @@
+﻿namespace Existence.Time
+{
+    internal class SpaceTimeContinuum
+    {
+        internal class Continuum
+        {
+            public static class Space { }
+            public static class Time { }
+        }
+    }
+
+    internal class UPS
+    {
+        public static class U
+        {
+            public static class United { }
+            public static class Universal { }
+        }
+        public static class P
+        {
+            public static class Postal { }
+            public static class Parcel { }
+        }
+        public static class Service { }
+    }
+
+    internal class IPT
+    {
+        public static class Intergalactic { }
+        public static class P
+        {
+            public static class Pulsar { }
+            public static class Postal { }
+            public static class Parcel { }
+        }
+        public static class Time { }
+    }
+}
