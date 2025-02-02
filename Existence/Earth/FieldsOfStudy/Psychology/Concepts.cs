43ecf3e85ed292491e4814d57f7b9683ec7a7388
Chris Rowe <destroyallpugs@gmail,com>
2025-02-02 13:34:49 -0600

    add more color information

--- Existence/Earth/FieldsOfStudy/Psychology/Concepts.cs
+++ Existence/Earth/FieldsOfStudy/Psychology/Concepts.cs
@@ -14,40 +14,53 @@ namespace Existence.Earth.FieldsOfStudy.Psychology
             public static class ColorsSeemBrighter { }
             public static class ColorsMeanThings
             {
-                public static class RainbowROYGBIV
+                public static class Rainbow
                 {
-                    public static class Red
+                    public static class PeopleHaveAWideRangeOfBehaviorSuchAsHomosexuality { }
+
+                    public static class AcronymROYGBIV
                     {
-                        public static class Stop { }
-                        public static class Important { }
-                    }
-                    public static class Orange
-                    {
-                        public static class Warning { }
-                    }
-                    public static class Yellow
-                    {
-                        public static class Caution { }
-                    }
-                    public static class Green
-                    {
-                        public static class Good { }
-                        public static class Grass { }
-                    }
-                    public static class Blue
-                    {
-                        public static class Calm { }
-                        public static class Safe { }
-                    }
-                    public static class Indigo
-                    {
-                        public static class PartOfTheRainbow { }
-                    }
-                    public static class Purple
-                    {
-                        public static class PersonalToSomeone { }
+                        public static class Red
+                        {
+                            public static class Stop { }
+                            public static class Important { }
+                        }
+                        public static class Orange
+                        {
+                            public static class Warning { }
+                        }
+                        public static class Yellow
+                        {
+                            public static class Caution { }
+                        }
+                        public static class Green
+                        {
+                            public static class Go { }
+                            public static class Good { }
+                            public static class Grass { }
+                        }
+                        public static class Blue
+                        {
+                            public static class Calm { }
+                            public static class Safe { }
+                        }
+                        public static class Indigo
+                        {
+                            public static class PartOfTheRainbow { }
+                        }
+                        public static class Purple
+                        {
+                            public static class PersonalToSomeone { }
+                        }
                     }
                 }
+                public static class TrafficLights
+                {
+                    public static class RedMeansStop { }
+                    public static class YellowMeansCaution { }
+                    public static class GreenMeansGo { }
+                }
+
                 public static class WhiteAndBlackOpposites
                 {
                     public static class HumanRaces { }
