6ce64e15b400033825b1e7781007552736956d6d
Chris Rowe <destroyallpugs@gmail,com>
2025-02-02 13:32:00 -0600

    add psychology concepts of apophenia and synethesia

---
+++ Existence/Earth/FieldsOfStudy/Psychology/Concepts.cs
@@ -0,0 +1,66 @@
+﻿
+using System.Xml.Schema;
+
+namespace Existence.Earth.FieldsOfStudy.Psychology
+{
+    public static class Concepts
+    {
+        public static class Apophenia
+        {
+            public static class MakingConnectionsWithApparentlyUnrelatedThings { }
+        }
+        public static class Synethesia
+        {
+            public static class ColorsSeemBrighter { }
+            public static class ColorsMeanThings
+            {
+                public static class RainbowROYGBIV
+                {
+                    public static class Red
+                    {
+                        public static class Stop { }
+                        public static class Important { }
+                    }
+                    public static class Orange
+                    {
+                        public static class Warning { }
+                    }
+                    public static class Yellow
+                    {
+                        public static class Caution { }
+                    }
+                    public static class Green
+                    {
+                        public static class Good { }
+                        public static class Grass { }
+                    }
+                    public static class Blue
+                    {
+                        public static class Calm { }
+                        public static class Safe { }
+                    }
+                    public static class Indigo
+                    {
+                        public static class PartOfTheRainbow { }
+                    }
+                    public static class Purple
+                    {
+                        public static class PersonalToSomeone { }
+                    }
+                }
+                public static class WhiteAndBlackOpposites
+                {
+                    public static class HumanRaces { }
+                    public static class White
+                    {
+                        public static class Life { }
+                    }
+                    public static class Black
+                    {
+                        public static class Death { }
+                    }
+                }
+            }
+        }
+    }
+}
