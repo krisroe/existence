2d5637026a21195bd87775466b3ca13b51c43cd1
Chris Rowe <destroyallpugs@gmail,com>
2025-02-02 08:21:58 -0600

    add earth IPT = Intergalactic pulsar time

--- Existence/Earth/Acronyms.cs
+++ Existence/Earth/Acronyms.cs
@@ -6,4 +6,8 @@ namespace Existence.Earth
         public static class UnitedParcelService { }
         public static class PackageDelivery { }
     }
+    public static class IPT
+    {
+        public static class IntergalacticPulsarTime { }
+    }
 }
