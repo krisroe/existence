4d05ff560a09bd0d2d502b1853ea1f2a9c83f8ff
Chris Rowe <destroyallpugs@gmail,com>
2025-02-02 13:46:48 -0600

    add holiday day information

--- Existence/Earth/Year.cs
+++ Existence/Earth/Year.cs
@@ -8,18 +8,81 @@
         }
         public static class Months
         {
-            public static class January { }
-            public static class February { }
-            public static class March { }
-            public static class April { }
+            public static class January 
+            {
+                public static class NewYearsDay
+                {
+                    public const int DayNumber = 1;
+                }
+            }
+            public static class February
+            {
+                public static class GroundhogDay
+                {
+                    public const int DayNumber = 2;
+                }
+                public static class ValentinesDay
+                {
+                    public const int DayNumber = 14;
+                }
+            }
+            public static class March
+            {
+                public static class SaintPatricksDay { }
+                public static class Equinox { }
+            }
+            public static class April 
+            {
+                public static class AprilFoolsDay
+                {
+                    public const int DayNumber = 1;
+                }
+            }
             public static class May { }
-            public static class June { }
-            public static class July { }
+            public static class June
+            {
+                public static class SummerSolsticsForNorthernHemisphere { }
+            }
+            public static class July
+            {
+                public static class UnitedStatesIndependenceDay
+                {
+                    public const int DayNumber = 1;
+                }
+            }
             public static class August { }
-            public static class September { }
-            public static class October { }
-            public static class November { }
-            public static class December { }
+            public static class September
+            {
+                public static class Equinox { }
+            }
+            public static class October
+            {
+                public static class ColumbusDay { }
+                public static class Halloween
+                {
+                    public const int DayNumber = 31;
+                }
+            }
+            public static class November 
+            {
+                public static class Thanksgiving { }
+            }
+            public static class December
+            {
+                public static class WinterSolsticeForNorthernHemisphere { }
+                public static class ChristmasEve
+                {
+                    public const int DayNumber = 24;
+                }
+                public static class ChristmasDay
+                {
+                    public const int DayNumber = 25;
+                }
+                public static class NewYearsEve
+                {
+                    public const int DayNumber = 31;
+                }
+            }
         }
     }
 }
