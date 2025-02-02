9f6a60791d332983e05438a558ee09129353b9b2
Chris Rowe <destroyallpugs@gmail,com>
2025-02-02 07:54:44 -0600

    add ipt

--- Existence/Word/Continuum.cs
+++ Existence/Word/Continuum.cs
@@ -5,4 +5,16 @@
         public static class Space { }
         public static class Time { }
     }
+
+    internal class IPT
+    {
+        public static class Intergalactic { }
+        public static class P
+        {
+            public static class Pulsar { }
+            public static class Postal { }
+            public static class Parcel { }
+        }
+        public static class Time { }
+    }
 }
