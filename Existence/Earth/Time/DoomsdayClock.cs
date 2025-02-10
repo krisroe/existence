fc0490711796112af23bb0fd3c564f38f9067fd8
Chris Rowe <destroyallpugs@gmail,com>
2025-02-10 08:35:25 -0600

    add doomsday clock

---
+++ Existence/Earth/Time/DoomsdayClock.cs
@@ -0,0 +1,308 @@
+﻿using Existence.Time;
+
+namespace Existence.Earth.Time
+{
+    /// <summary>
+    /// symbol that represents the estimated likelihood of a human-made global catastrophe, in the opinion of the 
+    /// members of the Bulletin of the Atomic Scientists. The Clock is a metaphor, not a prediction, 
+    /// for threats to humanity from unchecked scientific and technological advances. The time on the Clock is 
+    /// not to be interpreted as actual time. A hypothetical global catastrophe is represented by midnight 
+    /// on the Clock, with the Bulletin's opinion on how close the world is to one represented by a 
+    /// certain number of minutes or seconds to midnight, which is reassessed in January of each year.
+    /// </summary>
+    public static class DoomsdayClock
+    {
+        /// <summary>
+        /// initial setting
+        /// </summary>
+        [Year(1947)]
+        [TimeChangeMinutes(0, 0)]
+        [TimeDurationMinutes(7, 0)]
+        public static class v01 { }
+
+        /// <summary>
+        /// The Soviet Union tests its first atomic bomb, the RDS-1, starting the nuclear arms race.
+        /// </summary>
+        [Year(1949)]
+        [TimeChangeMinutes(-4, 0)]
+        [TimeDurationMinutes(3, 0)]
+        public static class v02 { }
+
+        /// <summary>
+        /// The United States tests its first thermonuclear device in November 1952 as part of Operation Ivy, 
+        /// before the Soviet Union follows suit with the Joe 4 test in August.
+        /// </summary>
+        [Year(1953)]
+        [TimeChangeMinutes(-1, 0)]
+        [TimeDurationMinutes(2, 0)]
+        public static class v03 { }
+
+        /// <summary>
+        /// In response to a perception of increased scientific cooperation and public understanding of the dangers of nuclear weapons
+        /// (as well as political actions taken to avoid "massive retaliation"), the United States and Soviet Union cooperate 
+        /// and avoid direct confrontation in regional conflicts such as the 1956 Suez Crisis, the 1958 Second Taiwan Strait Crisis, 
+        /// and the 1958 Lebanon crisis. Scientists from various countries help establish the International Geophysical Year, 
+        /// a series of coordinated, worldwide scientific observations between nations allied with both the United States 
+        /// and the Soviet Union, and the Pugwash Conferences on Science and World Affairs, which allow Soviet and American 
+        /// scientists to interact.
+        /// </summary>
+        [Year(1960)]
+        [TimeChangeMinutes(5, 0)]
+        [TimeDurationMinutes(7, 0)]
+        public static class v04 { }
+
+        /// <summary>
+        /// The United States and the Soviet Union sign the Partial Test Ban Treaty, limiting atmospheric nuclear testing.
+        /// </summary>
+        [Year(1963)]
+        [TimeChangeMinutes(5, 0)]
+        [TimeDurationMinutes(12, 0)]
+        public static class v05 { }
+
+        /// <summary>
+        /// The involvement of the United States in the Vietnam War intensifies, the Indo-Pakistani War of 1965 takes place, 
+        /// and the Six-Day War occurs in 1967. France and China, two nations which have not signed the 
+        /// Partial Test Ban Treaty, acquire and test nuclear weapons (the 1960 Gerboise Bleue and the 1964 596, respectively) 
+        /// to assert themselves as global players in the nuclear arms race.
+        /// </summary>
+        [Year(1968)]
+        [TimeChangeMinutes(-5, 0)]
+        [TimeDurationMinutes(7, 0)]
+        public static class v06 { }
+
+        /// <summary>
+        /// Every nation in the world, with the notable exceptions of India, Israel, and Pakistan, 
+        /// signs the Nuclear Non-Proliferation Treaty.
+        /// </summary>
+        [Year(1969)]
+        [TimeChangeMinutes(3, 0)]
+        [TimeDurationMinutes(10, 0)]
+        public static class v07 { }
+
+        /// <summary>
+        /// The United States and the Soviet Union sign the first Strategic Arms Limitation Treaty (SALT I) and the 
+        /// Anti-Ballistic Missile (ABM) Treaty.
+        /// </summary>
+        [Year(1972)]
+        [TimeChangeMinutes(2, 0)]
+        [TimeDurationMinutes(12, 0)]
+        public static class v08 { }
+
+        /// <summary>
+        /// India tests a nuclear device (Smiling Buddha), and SALT II talks stall. Both the United States and the 
+        /// Soviet Union modernize multiple independently targetable reentry vehicles (MIRVs).
+        /// </summary>
+        [Year(1974)]
+        [TimeChangeMinutes(-3, 0)]
+        [TimeDurationMinutes(9, 0)]
+        public static class v09 { }
+
+        /// <summary>
+        /// Unforeseeable end to deadlock in American–Soviet talks as the Soviet–Afghan War begins. 
+        /// As a result of the war, the U.S. Senate refuses to ratify the SALT II agreement.
+        /// </summary>
+        [Year(1980)]
+        [TimeChangeMinutes(-2, 0)]
+        [TimeDurationMinutes(7, 0)]
+        public static class v10 { }
+
+        /// <summary>
+        /// The Soviet war in Afghanistan toughens the U.S.' nuclear posture. U.S. President Jimmy Carter withdraws 
+        /// the United States from the 1980 Summer Olympic Games in Moscow. The Carter administration considers ways 
+        /// in which the United States could win a nuclear war. Ronald Reagan becomes President of the United States, 
+        /// scraps further arms reduction talks with the Soviet Union, and argues that the only way to end the Cold War 
+        /// is to win it. Tensions between the United States and the Soviet Union contribute to the danger of nuclear 
+        /// annihilation as they each deploy intermediate-range missiles in Europe. The adjustment also accounts for the 
+        /// Iran hostage crisis, the Iran–Iraq War, China's atmospheric nuclear warhead test, the declaration of 
+        /// martial law in Poland, apartheid in South Africa, and human rights abuses across the world.
+        /// </summary>
+        [Year(1981)]
+        [TimeChangeMinutes(-3, 0)]
+        [TimeDurationMinutes(4, 0)]
+        public static class v11 { }
+
+        /// <summary>
+        /// Further escalation of the tensions between the United States and the Soviet Union, with the ongoing Soviet–Afghan 
+        /// War intensifying the Cold War. U.S. Pershing II medium-range ballistic missile and cruise missiles are deployed 
+        /// in Western Europe.[31] Ronald Reagan pushes to win the Cold War by intensifying the arms race between the 
+        /// superpowers. The Soviet Union and its allies (except Romania) boycott the 1984 Olympic Games in Los Angeles, 
+        /// as a response to the U.S.-led boycott in 1980.
+        /// </summary>
+        [Year(1984)]
+        [TimeChangeMinutes(-1, 0)]
+        [TimeDurationMinutes(3, 0)]
+        public static class v12 { }
+
+        /// <summary>
+        /// In December 1987, the United States and the Soviet Union sign the Intermediate-Range Nuclear Forces Treaty, 
+        /// to eliminate intermediate-range nuclear missiles, and their relations improve.
+        /// </summary>
+        [Year(1988)]
+        [TimeChangeMinutes(3, 0)]
+        [TimeDurationMinutes(6, 0)]
+        public static class v13 { }
+
+        /// <summary>
+        /// The fall of the Berlin Wall and the Iron Curtain, along with the reunification of Germany, mean that the 
+        /// Cold War is nearing its end.
+        /// </summary>
+        [Year(1990)]
+        [TimeChangeMinutes(4, 0)]
+        [TimeDurationMinutes(10, 0)]
+        public static class v14 { }
+
+        /// <summary>
+        /// The United States and Soviet Union sign the first Strategic Arms Reduction Treaty (START I), 
+        /// the US announces the removal of many tactical nuclear weapons in September 1991, and the 
+        /// Soviet Union takes similar steps, as well as announcing the complete cessation of all 
+        /// nuclear testing in October 1991. The Bulletin editorial, published November 26, 1991, 
+        /// announces that "the 40-year-long East-West nuclear arms race is over." One month after 
+        /// the Bulletin made this clock adjustment, the Soviet Union dissolves on December 26, 1991. 
+        /// </summary>
+        [Year(1991)]
+        [TimeChangeMinutes(7, 0)]
+        [TimeDurationMinutes(17, 0)]
+        public static class v15 { }
+
+
+        /// <summary>
+        /// Global military spending continues at Cold War levels amid concerns about post-Soviet nuclear 
+        /// proliferation of weapons and brainpower.
+        /// </summary>
+        [Year(1995)]
+        [TimeChangeMinutes(-3, 0)]
+        [TimeDurationMinutes(14, 0)]
+        public static class v16 { }
+
+        /// <summary>
+        /// Both India (Pokhran-II) and Pakistan (Chagai-I) test nuclear weapons in a tit-for-tat show 
+        /// of aggression; the United States and Russia run into difficulties in further reducing 
+        /// stockpiles.
+        /// </summary>
+        [Year(1998)]
+        [TimeChangeMinutes(-5, 0)]
+        [TimeDurationMinutes(9, 0)]
+        public static class v17 { }
+
+        /// <summary>
+        /// Little progress on global nuclear disarmament. United States rejects a series of arms control 
+        /// treaties and announces its intentions to withdraw from the Anti-Ballistic Missile Treaty, amid 
+        /// concerns about the possibility of a nuclear terrorist attack due to the amount of weapon-grade 
+        /// nuclear materials that are unsecured and unaccounted for worldwide.
+        /// </summary>
+        [Year(2002)]
+        [TimeChangeMinutes(-2, 0)]
+        [TimeDurationMinutes(7, 0)]
+        public static class v18 { }
+
+        /// <summary>
+        /// North Korea tests a nuclear weapon in October 2006, Iran's nuclear ambitions, a renewed 
+        /// American emphasis on the military utility of nuclear weapons, the failure to adequately secure 
+        /// nuclear materials, and the continued presence of some 26,000 nuclear weapons in the 
+        /// United States and Russia. After assessing the dangers posed to civilization, climate change 
+        /// was added to the prospect of nuclear annihilation as the greatest threats to humanity.
+        /// </summary>
+        [Year(2007)]
+        [TimeChangeMinutes(-2, 0)]
+        [TimeDurationMinutes(5, 0)]
+        public static class v19 { }
+
+        /// <summary>
+        /// Worldwide cooperation to reduce nuclear arsenals and limit effect of climate change. The 
+        /// New START agreement is ratified by both the United States and Russia, and more negotiations 
+        /// for further reductions in the American and Russian nuclear arsenal are already planned. 
+        /// The 2009 United Nations Climate Change Conference in Copenhagen results in the developing 
+        /// and industrialized countries agreeing to take responsibility for carbon emissions and to 
+        /// limit global temperature rise to 2 degrees Celsius.
+        /// </summary>
+        [Year(2010)]
+        [TimeChangeMinutes(1, 0)]
+        [TimeDurationMinutes(6, 0)]
+        public static class v20 { }
+
+        /// <summary>
+        /// Lack of global political action to address global climate change, nuclear weapons stockpiles, 
+        /// the potential for regional nuclear conflict, and nuclear power safety.
+        /// </summary>
+        [Year(2012)]
+        [TimeChangeMinutes(-1, 0)]
+        [TimeDurationMinutes(5, 0)]
+        public static class v21 { }
+
+        /// <summary>
+        /// Concerns amid continued lack of global political action to address global climate change, the
+        /// modernization of nuclear weapons in the United States and Russia, and the problem of nuclear 
+        /// waste.
+        /// </summary>
+        [Year(2015)]
+        [TimeChangeMinutes(-2, 0)]
+        [TimeDurationMinutes(3, 0)]
+        public static class v22 { }
+
+        /// <summary>
+        /// United States President Donald Trump's comments over nuclear weapons, the threat of a renewed arms 
+        /// race between the U.S. and Russia, and the expressed disbelief in the scientific consensus over 
+        /// climate change by the Trump administration.
+        /// </summary>
+        [Year(2017)]
+        [TimeChangeMinutes(0, -30)]
+        [TimeDurationMinutes(2, 30)]
+        public static class v23 { }
+
+        /// <summary>
+        /// Failure of world leaders to deal with looming threats of nuclear war and climate change.
+        /// </summary>
+        [Year(2018)]
+        [TimeChangeMinutes(0, -30)]
+        [TimeDurationMinutes(2, 0)]
+        public static class v24 { }
+
+        /// <summary>
+        /// Reaffirmed current state per continuing climate change and Trump administration's abandonment 
+        /// of U.S. efforts to lead the world toward decarbonization; U.S. withdrawal from the Paris Agreement, 
+        /// the Joint Comprehensive Plan of Action, and the Intermediate-Range Nuclear Forces Treaty; 
+        /// U.S. and Russian nuclear modernization efforts; information warfare threats and other dangers 
+        /// from "disruptive technologies" such as synthetic biology, artificial intelligence, and cyberwarfare.
+        /// </summary>
+        [Year(2019)]
+        [TimeChangeMinutes(0, 0)]
+        [TimeDurationMinutes(2, 0)]
+        public static class v25 { }
+
+        /// <summary>
+        /// Current issues are "the most dangerous situation that humanity has ever faced", citing
+        /// failure of world leaders to deal with the increased threats of nuclear war, such as the end of the 
+        /// Intermediate-Range Nuclear Forces Treaty (INF) between the United States and Russia as well as 
+        /// increased tensions between the U.S. and Iran, along with the continued neglect of climate change.
+        /// </summary>
+        [Year(2020)]
+        [TimeChangeMinutes(0, -20)]
+        [TimeDurationMinutes(1, 40)]
+        public static class v26 { }
+
+        /// <summary>
+        /// Due largely–but not exclusively–to the Russian invasion of Ukraine and the increased risk of nuclear 
+        /// escalation stemming from the conflict. Russia suspended its participation in the last remaining 
+        /// nuclear weapons treaty between it and the United States, New START. Russia also brought its war to 
+        /// the Chernobyl and Zaporizhzhia nuclear reactor sites, violating international protocols and 
+        /// risking widespread release of radioactive materials. North Korea resumed its nuclear rhetoric, 
+        /// launching an intermediate-range ballistic missile test over Japan in October 2022. Continuing 
+        /// threats posed by the climate crisis and the breakdown of global norms and institutions set up 
+        /// to mitigate risks associated with advancing technologies and biological threats such as COVID-19 
+        /// also contributed.
+        /// </summary>
+        [Year(2023)]
+        [TimeChangeMinutes(0, -10)]
+        [TimeDurationMinutes(1, 30)]
+        public static class v27 { }
+
+        /// <summary>
+        /// The continuing Russian invasion of Ukraine and the Middle Eastern crisis, increased nuclear 
+        /// proliferation, effects of climate change, biological threats, and advancing technologies.
+        /// </summary>
+        [Year(2025)]
+        [TimeChangeMinutes(0, -1)]
+        [TimeDurationMinutes(1, 29)]
+        public static class v28 { }
+    }
+}
