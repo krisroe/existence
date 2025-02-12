a28ec662de0000d4f9fe986d884dea40c7e15e5f
Chris Rowe <destroyallpugs@gmail,com>
2025-02-12 11:57:57 -0600

    add astrology timeline

---
+++ Existence/Earth/FieldsOfStudy/Astrology/AstrologyTimeline.cs
@@ -0,0 +1,38 @@
+﻿
+namespace Existence.Earth.FieldsOfStudy.Astrology
+{
+
+    public class Capricorn20242025
+    {
+        /// <summary>
+        /// 1/18/2025 10:30 PM EST
+        /// (in compliance with Protecting Americans from Foreign Adversary Controlled Applications Act (PAFACA))
+        /// </summary>
+        public static class TikTokSuspendedUSOperations { }
+
+        /// <summary>
+        /// 1/19/2025 12:00 PM EST
+        /// (following assurances from President-elect Donald Trump that he would grant an extension upon taking office)
+        /// TikTok was inaccessible for ~13.5 hours
+        /// </summary>
+        public static class TikTokRestoredUSOperations { }
+    }
+
+    /// <summary>
+    /// 1/19/2025 20:00 UTC (1/19/2025 3:00 PM EST)
+    /// </summary>
+    public static class Aquarius2025
+    {
+        /// <summary>
+        /// 2/9/2025
+        /// </summary>
+        public static class SuperBowl59 { }
+    }
+
+    /// <summary>
+    /// 2/18/2025 10:06 UTC
+    /// </summary>
+    public static class Pisces2025
+    {
+    }
+}
