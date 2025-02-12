85c9c4183a6b3d7042bf431473a69c0f11952ff0
Chris Rowe <destroyallpugs@gmail,com>
2025-02-12 12:34:58 -0600

    add football games to astrology timeline

--- Existence/Earth/FieldsOfStudy/Astrology/AstrologyTimeline.cs
+++ Existence/Earth/FieldsOfStudy/Astrology/AstrologyTimeline.cs
@@ -5,6 +5,11 @@ namespace Existence.Earth.FieldsOfStudy.Astrology
     public class Capricorn20242025
     {
         /// <summary>
+        /// 1/17/2025 upholds PAFACA against first amendment challenge
+        /// </summary>
+        public static class USSupremeCourtRulesInTikTokvGarland { }
+
+        /// <summary>
         /// 1/18/2025 10:30 PM EST
         /// (in compliance with Protecting Americans from Foreign Adversary Controlled Applications Act (PAFACA))
         /// </summary>
@@ -23,6 +28,31 @@ namespace Existence.Earth.FieldsOfStudy.Astrology
     /// </summary>
     public static class Aquarius2025
     {
+        /// <summary>
+        /// started at the same time as Aquarius (1/19/2025 3:00 PM)
+        /// </summary>
+        public static class NFLRamsVsEaglesDivisionalGame { }
+
+        /// <summary>
+        /// started 1/19/2025 6:30 PM
+        /// </summary>
+        public static class NFLRavensVsBillsDivisionalGame { }
+
+        /// <summary>
+        /// 1/20/2025 12:00 EST
+        /// </summary>
+        public static class DonaldTrumpBecomesPresident { }
+
+        /// <summary>
+        /// 1/26/2025 3:00 EST
+        /// </summary>
+        public static class NFCConferenceChampionshipCommandersvsEagles { }
+
+        /// <summary>
+        /// 1/26/2025 6:30 EST 
+        /// </summary>
+        public static class AFCConferenceChampionshipBillsvsChiefs { }
+
         /// <summary>
         /// 2/9/2025
         /// </summary>
