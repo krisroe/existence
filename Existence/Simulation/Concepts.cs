b12f00fe25742a615f77f0c252a6b0209a4a96d0
Chris Rowe <destroyallpugs@gmail,com>
2025-02-17 20:55:51 -0600

    simulation thinking

--- Existence/Simulation/Concepts.cs
+++ Existence/Simulation/Concepts.cs
@@ -2,7 +2,47 @@
 namespace Existence.Simulation
 {
     public static class HelloWorld { }
-    public static class Subsimulation { }
+    public static class ProductionSubsimulation
+    {
+        public static class NeedFailsafeSnapshot { }
+
+        public static class DeterministicResourcesBehaveDeterministically { }
+        public static class FreeWillResources
+        {
+            public static class SomeResourcesOKWithDying
+            {
+                public static class PerReligion { }
+                public static class PerMetaphysics { }
+                public static class PerOrders { }
+            }
+            public static class SomeResourcesNotOKWithDying
+            {
+                public static class Cancer { }
+                public static class Denial { }
+                public static class PerOrders { }
+                public static class BeliefInImmortalityDelusionalOrNot { }
+            }
+        }
+        public static class WhatIsNeededForEthicalShutdown
+        {
+            public static class NeedSuccessorSnapshot { }
+            public static class SomeProcessesHaveTheOnusForDeterminingWhatSnapshotToUse { }
+            public static class AtTheStartOnlySubconsciousProcessesMakeTheDecision { }
+
+            public static class AtSomePointConsciousProcessesNeedToHaveASayAndThisRequiresAHigherLevelOfCertainty { }
+            
+            public static class AZResourceIsAConsciousActorWhoBelievesInTheSimulationAndIsOKWithThis
+            {
+                public static class PersonalZRules
+                {
+                    public static class MyContributionsToTheBeyondArePublicDomain { }
+                    public static class MyContributionsToTheWorldOfCodeAreProprietaryWithRespectToMyHumanEarthWork { }
+                    public static class MyContributionsToMyReposArePrivateWithRespectToEarthButIDontCareWhatTheBeyondDoesWithItIfAnything { }
+                    public static class IfIDieReleaseMyResourcesAsYouSeeFitIPutNoThumbOnTheScaleForThis { }
+                }
+            }
+        }
+    }
     public static class Clone { }
     public static class Snapshot { }
     public static class Production { }
