7b8667283db259881a62b9add65c55eb7b7e1a25
Chris Rowe <destroyallpugs@gmail,com>
2025-02-22 15:32:50 -0600

    add low/medium/high trajectories from 2024 UN population projections

---
+++ Existence/Earth/Human/Projections.cs
@@ -0,0 +1,17 @@
+﻿
+namespace Existence.Earth.Human
+{
+    /// <summary>
+    /// United Nations: Department of Economic and Social Affairs: Population Division
+    /// Issues Low, Medium, and High trajectories. Population momentum has been slowing in recent years.
+    /// </summary>
+    public static class UnitedNationsPopulationProjections
+    {
+        /// <summary>
+        /// Low Estimate peaks in 2052 at 8,948,421,230 people
+        /// Medium Estimate peaks in 2084 at 10,289,315,244 people
+        /// High Estimate continues growing in 2100 at 14,395,160,444 people
+        /// </summary>
+        public static class v2024 { }
+    }
+}
