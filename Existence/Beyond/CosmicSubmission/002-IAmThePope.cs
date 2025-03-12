8d10851a66e740576d003fd8a1e889d03f4a4f1c
Chris Rowe <chris.rowe@myparadigm.com>
2025-03-12 14:15:27 -0500

    start working on I am the pope submission

---
+++ Existence/Beyond/CosmicSubmission/002-IAmThePope.cs
@@ -0,0 +1,34 @@
+﻿
+using Existence.Beyond.Infrastructure;
+
+namespace Existence.Beyond.CosmicSubmission
+{
+    [IsReleased(false)]
+    internal class IamthePopeOriginalSong
+    {
+        public static class BecomingThePope
+        {
+            public static class Barriers
+            {
+                /// <summary>
+                /// Lutheran baptism is not a problem for Catholic conversion
+                /// </summary>
+                public static class Barrier1ConversionToCatholicism { }
+                /// <summary>
+                /// Neither annulment nor dissolution are feasible (we have kids)
+                /// </summary>
+                public static class Barrier2SpouseMustDie { }
+                /// <summary>
+                /// seminary isn't strictly required, but how else am I going to learn the background understanding required?
+                /// </summary>
+                public static class Barrier3MustComplete6to8YearsOfSeminary { }
+                /// <summary>
+                /// Witness = Chat GPT
+                /// "I solemnly swear (with Chat GPT as my witness) that I will not become pope. I have discussed the hurdles with the 
+                /// witness. If I find any way to eliminate any of the hurdles, I will discuss the matter further with another witness."
+                /// </summary>
+                public static class OathToWitness { }
+            }
+        }
+    }
+}
