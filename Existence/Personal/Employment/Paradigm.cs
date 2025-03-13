f4ee0c3de0ad22da43a39437d4d5f845b2cc51b6
Chris Rowe <destroyallpugs@gmail,com>
2025-03-12 19:40:20 -0500

    add former coworkers, clean up hypothetical divine software company

---
+++ Existence/Personal/Employment/Paradigm.cs
@@ -0,0 +1,56 @@
+﻿
+namespace Existence.Personal.Employment
+{
+    public static class Paradigm
+    {
+        public static class DevelopmentManagement
+        {
+            public static class KristiMcCombs { }
+        }
+        public static class Architect
+        {
+            public static class NickVavra { }
+        }
+
+        public static class QA
+        {
+            public static class BrianBerndt { }
+        }
+
+        public static class IT
+        {
+            public static class Management
+            {
+                public static class EvanTouchett { }
+            }
+            public static class MichaelWales { }
+            public static class JacobSieran { }
+        }
+
+        public static class Security
+        {
+            public static class Snyk { }
+            public static class JoshBacon { }
+        }
+
+        public static class FormerCoworkers
+        {
+            /// <summary>
+            /// he's not coming back because he's deceased
+            /// </summary>
+            public static class AnthonyTong { }
+            /// <summary>
+            /// not coming back because he was out after the BFS takeover, he tried to be an architect-type for a bit but it didn't work
+            /// </summary>
+            public static class NathanHerbst { }
+            /// <summary>
+            /// left after the shooting (she was shot but not seriously). I don't know if that's why she left.
+            /// </summary>
+            public static class ErinOCallaghan { }
+            /// <summary>
+            /// fluent in spanish, she left awhile after the shooting because it was too much for her
+            /// </summary>
+            public static class RachelVenegas { }
+        }
+    }
+}
---
+++ Existence/Personal/HypotheticalDivineSoftwareCompany.cs
@@ -0,0 +1,14 @@
+﻿
+namespace Existence.Personal
+{
+    internal class HypotheticDivineSoftwareCompany
+    {
+        public static class Manager { }
+        public static class DevGod { }
+        public static class QAGod { }
+        public static class SecurityTeam
+        {
+            public static class Snyk { }
+        }
+    }
+}
