a09ed15f59b8a6b9b10f916d2f2af58fbb8c9b92
Chris Rowe <chris.rowe@myparadigm.com>
2025-03-16 14:05:39 -0500

    fill out some details about the players in the catholic church

---
+++ Existence/Earth/Religions/Catholicism.cs
@@ -0,0 +1,44 @@
+﻿
+namespace Existence.Earth.Religions
+{
+    /// <summary>
+    /// independent sovereign state created by the Lateran Treaty of 1929 between the Holy See and Italy
+    /// Ensures the Pope is not subject to any other nation's authority.
+    /// </summary>
+    public static class VaticanCity
+    {
+        /// <summary>
+        /// Sovereign (King/Monarch) of Vatican City
+        /// </summary>
+        public static class Pope { }
+    }
+
+    internal class CatholicChurch
+    {
+        /// <summary>
+        /// central governing institution of the cathoic church, especially in matters of diplomacy and international law
+        /// </summary>
+        public static class HolySee
+        {
+            /// <summary>
+            /// Bishop of Rome and Supreme Pontiff
+            /// </summary>
+            public static class Pope { }
+
+            /// <summary>
+            /// the administrative departments assisting the pope
+            /// </summary>
+            public static class RomanCuria { }
+
+            /// <summary>
+            /// bishopric of the pope
+            /// </summary>
+            public static class SeeOfRome { }
+        }
+
+        /// <summary>
+        /// https://en.wikipedia.org/wiki/List_of_current_cardinals
+        /// </summary>
+        public static class CollegeOfCardinals { }
+    }
+}
