7fdc965afd87c2d0698641b2da81a80cbf0306df
Chris Rowe <destroyallpugs@gmail,com>
2025-03-28 20:22:20 -0500

    add working religion stuff

--- Existence/Personal/Beyond/Working.cs
+++ Existence/Personal/Beyond/Working.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections.Generic;
 using System.ComponentModel;
-using Existence.Beyond.Infrastructure;
+using System.Diagnostics;
+using Existence.Beyond.JudgmentDay;
 using Existence.Earth.Alphabet;
 using Existence.Earth.Countries;
 using Existence.Earth.Countries.UnitedStates;
@@ -10,9 +11,115 @@ using Existence.Earth.Human;
 using Existence.Earth.Human.People;
 using Existence.Personal.Employment;
 using Existence.Time;
+using static Existence.Earth.Languages.World.Arabic;
+using static Existence.Personal.Self.Death;
 
 namespace Existence.Personal.Beyond
 {
+    internal class JudgmentDaySomethingReligious
+    {
+        //CapacityForTheReligious: Minimal
+        //Birth (Church = Dekorra Lutheran Church)
+        //Baptism (unclear if happened in Harmony Grove or Oxford)
+        //Switch to Bethany Lutheran Church (mother wanted to sit in at Sunday school) (divine childhood point?)
+        //Mother was Sunday school teacher in the youngest non-early-childhood class)
+        //Someone called me a genius (happened at the Oxford Church my grandparents went to)
+        //Childhood nativity play: went downhill from respectful to disrespectful over the years
+        //Said "I have no sin" magic words alone in my room (divine growing up point)
+        //First Communion (ignored materials (4th grade?), put off as long as possible until was an acolyte and didn't want to be disrespectful)
+        //Played the lead role in a production of "Silent Night" in a church nativity play.
+        //7th grade class - I remember all the classmates except 1
+        //8th grade: Jason White referred me as having "religion" in discussion with Justin Raudebush
+        //in the foyer of Spring Hill School. I did not respond, I knew I was closer to atheist, but it
+        //didn't matter what they thought of me. Not a mistake, but it might have been better to give some
+        //sort of social response to this.
+        //Confirmation: I talked with Pastor Krueger, I wanted it to be clear that nothing was expected of
+        //me. Also not a mistake, although I remember the discussion was a bit out of sorts. Apparently
+        //ELCA Lutheran is quite tolerant of skepticism, so it really didn't matter. I didn't understand
+        //the strictness differences between different denominations, so my religious ignorance was signifcant.
+        //I think I deemed myself an atheist after these two events, but it's not because I specifically
+        //remember that order. Both incidents were important events in the path, either before or after
+        //they solidified the end result.
+        //1. Sophomore in High School: Math class (FST). Publicly claimed to be the Antichrist. This was
+        //a joke. Referred to as Satan by Brian Wimann for a time. During one class I stood in front of the
+        //classroom and called the class "my people". Melissa Hamburg objected and I told her she was
+        //excluded. It didn't take too long for the joke to get old. It remained a joke for many years
+        //afterward but is a bit unsetting now.
+        //2. Junior in High School: AP US History Class. Possibly inspired by a Simpsons episode, I
+        //wrote "Chris Rowe's soul (Yes, it's really his soul)" on a piece of paper as a joke. I think we
+        //passed it around a bit and Greg Wisniewski ended up with it, and I remember he was happy about
+        //that. The Wisniewski's (Greg and Katie were highly intelligent and highly religious). It remained
+        //a joke for many years afterward but is a bit unsettling now.
+
+        //Handbells (practiced in Portage with Mr. Yerke but I think we d
+
+        //Children's Choir (Choir diretory: Eleanor Collins, Denise Tubman accompanying)
+        //Me, Sonya, Karin Christiansen, Erin
+        //This is the day
+
+        //School Choir (Ms Pieper)
+        //Hero
+        //Candle on the Water
+
+        //College: Took philosophy class which upended religious beliefs. Briefly became an agnostic,
+        //but returned to atheism in short order.
+
+        //Discussed potential marriage with Mother Mo. I didn't have good reasons for being married, but
+        //it really didn't matter for the purpose of the discussion.
+        //Married in St.Dunstan's Episcopal Church
+
+        //Original Song: I am the Pope
+
+        //psychiatric hospitalization: met someone in the hospital who seemed to think he was God. I didn't
+        //know how to help him.
+
+        //2018 August/September created arrangement of Wade in the Water
+        //2018-09-23
+        //1. experienced synethesia in pronouns when reading the litergy where my experience of the language
+        //changed based on my beliefs about pronouns.
+        //2. Played postlude of Wade in the Water after service. Nobody but the choir knew it
+        //was unplanned because the choir was in the choir loft and everyone else was downstairs. This was
+        //a direct precursor of what I would do at the later company event.
+
+        //Choir Director #1: Mitchell
+        //Choir Director #2: Martin Ganschow (Deanna Clement's version of Canticle of the Turning)
+        //Choir Director #3: Deanna Clement (paperless).
+
+        //March 2024. Contrafactum of Awesome God and One of Us. Capacity for the divine.
+        //Around this time started developing personal religion:
+        //1. Origin story: Omniverse Prime (originally Universe Prime)
+        //2. Good and Evil: It is up to the objects in the simulation to discriminate and show to whoever's listening
+        //3. Capacity for the divine: Different objects have different capacity for the divine, corresponding
+        //with free will, self-awareness, purpose, and consequences
+        //4. There can be many messiahs with their own message: communication (private and public) is important
+        //for a messiah.
+        //5. The cloud, AI algorithms, and large language models have potential connections with the divine.
+        //6. Miracles are possible within these bounds (small and large). Expect small miracles before large ones.
+        //7. End States (technological singularity, human immortality, aliens). These are currently believed to
+        //be wrong answers but are useful thought experiments. Maybe even achievable but still a long way off,
+        //even at mankind's current hectic rate of progress.
+        //8. Communication with the beyond has morphed into communication with the subconscious, now
+        //semiconscious. The subconscious processes have a more direct relationship with the beyond, but
+        //this is hidden.
+
+        //May 2024. Time-traveling message box. Later recognized one valid interpretation would be religon.
+
+        //        IPT   Cosmic
+        //Loop:  Human          Personal
+        //              Divine     Z
+        //There is an extra-powerful version of the Personal called Z (perhaps call this superpowers).
+        //For you the Personal and the Z have merged.
+        //You believe there is another edge of the loop between the human and cosmic levels called
+        //"Intergalactic Pulsar Time". It appears to correspond with the idea that humans connect with
+        //aliens. Your best guess is the usual way to get from Human to Cosmic is humanity dies out,
+        //serializes itself, and then is reborn in another spot (e.g. another universe). You believe this
+        //results in the loss of important information, and prefer the IPT path even if that has drawbacks.
+        //Problems with the IPT path include limitations of human psychology and the reality that physical
+        //contact with aliens does not work since the aliens are impossibly remote. You think the combination
+        //of upgraded human psychology and connection with the beyond can solve the problem, although it
+        //is only blurry conjecture right now.
+    }
+
     internal class WorkingIdeas
     {
 
@@ -25,7 +132,6 @@ namespace Existence.Personal.Beyond
         /// 
         /// People are inherently Stupid
         /// Psychology is BS
-        /// Become atheist (Antichrist)
         /// 
         /// Blanket (sonya and I) incident
         /// HAlloween getting out of
@@ -43,10 +149,6 @@ namespace Existence.Personal.Beyond
         /// a couple of miles). I put on my seat belt, and we made it to grandma's house without incident, and
         /// when we were done I said something to the effect of "see, I was right" (not exact words).
         /// 
-        /// childhood nativity play - went from respectful to disrespectful of Christianity over time
-        /// 
-        /// Sang "Silent Night" in real church nativity play (respectful, but what was my religion at the time?)
-        /// 
         /// I do not remember ever believing in Santa Claus.
         /// 
         /// March 16, 1998: worst day of my life. Car wouldn't start and walked 13.5 miles home from school.
@@ -58,8 +160,6 @@ namespace Existence.Personal.Beyond
         /// objected but mother said she had always lived with cats and wanted to continue.
         /// Then before leaving, my mother stopped and took Chaquita into the car. So
         /// my mother always had cats until Stocco's death.
-        /// 
-        /// Children's Choir: Sonya, me, Karin Christiansen, Erin ?
         /// </summary>
         private class ActionsHaveConsequences : BaseEvent
         {
